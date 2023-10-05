3a6e4d57224912bbf64fec1e7cbe1e5ea216409c
Joseph Demcher <joedemcher@gmail.com>
2023-10-05 18:02:35 -0400

    fix update logic to retain task info

--- update_overdue_tasks_to_today.py
+++ update_overdue_tasks_to_today.py
@@ -1,8 +1,17 @@
 from todoist_api_python.api import TodoistAPI
+from dotenv import load_dotenv
+from datetime import datetime
+
 import json
 import requests
 import subprocess
 import sys
+import os
+import pprint
+
+load_dotenv()
+
+TODOIST_TOKEN = os.getenv("TODOIST_TOKEN", "")
 
 class Todoist:
     def __init__(self, token):
@@ -37,8 +46,10 @@ class Todoist:
         return self.api.get_tasks(filter=filterStr)
 
     def update_tasks_to_today(self, tasks):
+        today = datetime.today()
+        todayDate = today.strftime("%Y-%m-%d")
         for task in tasks:
-            self.api.update_task(task_id=task.id, due_string="Today")
+            self.api.update_task(task_id=task.id, due_string=task.due.string, due_date=todayDate)
             print("Task: '" + task.content + "' has been moved to today")
 
     def send_notification(self, title, message, priority, tags):
@@ -51,13 +62,12 @@ class Todoist:
             })
 
 def main():
-    token = 'f7bc5c37b3e3a745a8f375ff9f265b97b24ef468'
-    todoist = Todoist(token)
+    todoist = Todoist(TODOIST_TOKEN)
     try:
         overdue_tasks = todoist.get_filtered_tasks("overdue")
         if len(overdue_tasks) != 0:
             todoist.update_tasks_to_today(overdue_tasks)
-            todoist.send_notification("Tasks successfully moved", "Yesterday's tasks have been moved to today.", "high", "pushpin")
+            todoist.send_notification("Tasks successfully moved", "Yesterday's tasks have been moved to today.", "low", "pushpin")
     except Exception as error:
         todoist.send_notification("Tasks could not be moved", "Yesterday's tasks have not been moved to today.", "high", "no_entry_sign")
         print(error)
